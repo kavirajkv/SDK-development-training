9ddd8c8a733aa1a06d06cc6ae2ba01cfcaae834a
kavirajkv <kavirajk36kv@gmail.com>
2025-02-23 23:32:42 +0530

    digital sign APIs added

---
+++ digitalsign/go.mod
@@ -0,0 +1,7 @@
+module digitalsign
+
+go 1.22.12
+
+require github.com/kavirajkv/security v0.0.0-20250223070514-6eeddc71c4a1
+
+require github.com/gorilla/mux v1.8.1 
---
+++ digitalsign/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+
+	"github.com/kavirajkv/security/digest"
+	"github.com/kavirajkv/security/sign"
+)
+
+// to handle /getDigitalsign request body
+type message struct {
+	Message string `json:"message"`
+}
+
+// response body of /getDigitalsign & to handle verifySign requst body
+type signature struct {
+	Message   string `json:"message"`
+	Sign      string `json:"digital_sign"`
+	Publickey string `json:"publickey"`
+}
+
+// response body of verifySign
+type verification struct {
+	Status string `json:"status"`
+	Valid  bool   `json:"valid"`
+}
+
+// gets message as json body and returns sign,publickey,message
+func getDigitalsign(w http.ResponseWriter, r *http.Request) {
+	var message message
+
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, "Enter string message", http.StatusBadRequest)
+	}
+
+	public, private, _ := sign.GenerateKeypair()
+	msgdigest := digest.ShaDigest(message.Message) //SHA256 to create digest
+
+	sign := sign.Digitalsign(private, msgdigest)
+
+	response := signature{Message: message.Message, Sign: sign, Publickey: public}
+
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// gets sign,publickey and message to verify validity of the signature
+func verifySign(w http.ResponseWriter, r *http.Request) {
+	var digitalsign signature
+
+	err := json.NewDecoder(r.Body).Decode(&digitalsign)
+	if err != nil {
+		http.Error(w, "Enter ", http.StatusBadRequest)
+	}
+
+	msgdigest := digest.ShaDigest(digitalsign.Message)
+
+	verified := sign.Verifysign(digitalsign.Publickey, msgdigest, digitalsign.Sign)
+
+	//returns verification status
+	if verified {
+		successresponse := verification{Status: "Signature verified successfully - Valid", Valid: true}
+		json.NewEncoder(w).Encode(successresponse)
+	} else {
+		failureresponse := verification{Status: "Signature not verified - Invalid", Valid: false}
+		json.NewEncoder(w).Encode(failureresponse)
+	}
+
+}
+
+// routes to handle
+func route() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/getdigitalsign", getDigitalsign).Methods("POST", "OPTIONS")
+	router.HandleFunc("/verifysign", verifySign).Methods("POST", "OPTIONS")
+
+	return router
+}
+
+func main() {
+	r := route()
+	fmt.Println("Server running at port 8000")
+	http.ListenAndServe(":8000", r)
+}
